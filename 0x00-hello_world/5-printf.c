c76303ca1f290034155da1d190681364621838de
aziinnova <aziazinnova>
2022-06-30 13:15:15 -0700

    c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
  *  * main - Prints out a string to stdout.
- *   * Return: 0 if success.
- *    */
+ *   * Return: 0 if success.*/
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
