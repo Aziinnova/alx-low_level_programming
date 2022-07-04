b800de8a0328f2b20dc2d165494f78693b7e6a24
aziinnova <aziazinnova>
2022-07-04 14:00:49 -0700

    c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,13 @@
-i#include <stdio.h>
-/**
- *  * main - prints the alphabet in lowercase.
- *  (*
- *   * Return: 0 on success
- *    */
-int main(void)
-{
-		char c = 'a';
-
-			while (c <= 'z')
-					{
-								putchar(c);
-										c++;
-											}
-				putchar('\n');
-					return (0);
+#include <stdio.h>
+/*main - prints the 
+ * return : 0
+ */
+int main (void)
+{ char c = 'a';
+	while (c <= 'z');
+	{ putchar(c);
+		c++;
+	}
+	putchar('\n');
+	return(0);
 }
