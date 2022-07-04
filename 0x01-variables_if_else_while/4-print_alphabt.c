14ec4602f76021281dca6d111a57347c9b56e16f
aziinnova <aziazinnova>
2022-07-04 14:23:05 -0700

    c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,7 +3,7 @@
  *  * main - Prints the alphabet except the letters q and e.
  *  (*
  *   * Return: 0 on success
- *    */
+     */
 int main(void)
 {
 		char c = 'a';
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,7 +3,7 @@
  *  * main - prints the alphabet in reverse.
  *  (*
  *   * Return: 0 on success
- *    */
+     */
 int main(void)
 {
 		char c = 'z';
