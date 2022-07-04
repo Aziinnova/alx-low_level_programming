291b25cbf8e0fb1f63238f5f669d1a8093c3ed89
aziinnova <aziazinnova>
2022-07-04 14:49:29 -0700

    c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -3,14 +3,14 @@
  *  * main - Prints the alphabet except the letters q and e.
  *  (*
  *   * Return: 0 on success
-     */
+    */
 int main(void)
 {
 		char c = 'a';
 
-			while (c <= 'z')
+			while(c <= 'z')
 					{
-								if (c != 'q' && c != 'e')
+								if(c != 'q' && c != 'e')
 											{
 															putchar(c);
 																	}
