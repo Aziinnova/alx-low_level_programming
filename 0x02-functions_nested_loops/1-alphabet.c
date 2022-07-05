db7c3abd4c017ef4baf6dbf4870e75a767cf246c
aziinnova <aziazinnova>
2022-07-05 13:41:05 -0700

    c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,19 @@
 #include "main.h"
 /**
  *  * print_alphabet - prints alphabet
-    */
+    *
 void print_alphabet()
 {
 		char a;
 
-			for (a = 'a'; a <= 'z'; a++)
+			for (a = '; a <= '; a++)
 						_putchar(a);
 				_putchar('\n');
+}
+*/
+int main() {
+	    char c;
+	        for (c = 'a'; c <= 'z'; ++c)
+			        printf("%c ", c);
+		    return 0;
 }
