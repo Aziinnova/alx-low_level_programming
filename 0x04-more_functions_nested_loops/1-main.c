11fbb337b0f74a3de65852d197250a77accaa337
aziinnova <aziazinnova>
2022-07-08 12:45:46 -0700

    c

--- 0x04-more_functions_nested_loops/1-main.c
+++ 0x04-more_functions_nested_loops/1-main.c
@@ -1,8 +1,18 @@
-#ifndef MAIN_H                                                                                                    
-#define MAIN_H                                                                                                    
-#include <stdio.h>                                                                                                
-void positive_or_negative(int i);                                                                                 
-int largest_number(int a, int b, int c);                                                                          
-void print_remaining_days(int month, int day, int year);                                                          
-int convert_day(int month, int day);                                                                              
-#endif         
+#include "mains.h"
+#include <stdio.h>
+
+/**
+ *  * main - check the code for Holberton School students.
+ *   *
+ *    * Return: Always 0.
+      */
+int main(void)
+{
+	    char c;
+
+	        c = '0';
+		    printf("%c: %d\n", c, _isdigit(c));
+		        c = 'a';
+			    printf("%c: %d\n", c, _isdigit(c));
+			        return (0);
+}
