56fea88a920bf14f2fc0543c83d45f633c4e0699
aziinnova <aziazinnova>
2022-08-12 13:55:19 -0700

    c

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,21 +1,21 @@
-#include "lists.h"
+nclude "lists.h"
 
 /**
- *  * list_len - finds the number of elements in a linked list
- *   *@h: pointer to the list
- *    *
- *     *Return: the number of elements
-      */
+ *  * list_len - Calculate the number of elements.
+ *   * @h: Pointer to a list.
+ *    * Return: Integer.
+      **/
 
 size_t list_len(const list_t *h)
-		{
-					size_t elements = 0;
+{
+		const list_t *temp;
+			unsigned int counter = 0;
 
-							while (h)
-										{
-														elements++;
-																	h = h->next;
-																			}
-
-									return (elements);
-										}
+				temp = h;
+					while (temp)
+							{
+										counter++;
+												temp = temp->next;
+													}
+						return (counter);
+}
