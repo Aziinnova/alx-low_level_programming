5212cfb46f6810ae4e22f901cb09c7025978c992
Aziinnova <natanaba8@gmail.com>
2023-01-12 13:32:41 +0300

    low

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,28 +1,14 @@
 #include "main.h"
-#include <stdio.h>
-
 /**
- *  * print_diagonal - Print diagonal line dependent on the integer n.
- *   * @n : The number of lines using '\' characters to use
- *    * Return: Void.
-      */
-void print_diagonal(int n)
+*print_line - prints a straight line
+*@n:parameter
+*Return:returns nothing
+*/
+void print_line(int n)
 {
-		int i;
-			int spaces;
-
-				for (i = 0; i < n; i++)
-						{
-									for (spaces = 0; spaces < i; spaces++)
-												{
-																_putchar(' ');
-																		}
-											_putchar('\\');
-													_putchar('\n');
-														}
-
-					if (n <= 0)
-							{
-										_putchar('\n');
-											}
+while (n-- > 0)
+{
+_putchar('_');
+}
+_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,25 +2,25 @@
 #include <stdio.h>
 
 /**
- *  * print_square - Print square line dependent on the integer n.
- *   * @n : The number of lines using '#' characters to use per row and column
- *    * Return: Void.
-      */
+ * print_square - Print square line dependent on the integer n.
+ * @n : The number of lines using '#' characters to use per row and column
+ * Return: Void.
+ */
 void print_square(int n)
 {
-		int x;
-			int y;
+	int x;
+	int y;
 
-				for (y = 0; y < n; y++)
-						{
-									for (x = 0; x < n; x++)
-												{
-																_putchar('#');
-																		}
-											_putchar('\n');
-												}
-					if (n <= 0)
-							{
-										_putchar('\n');
-											}
+	for (y = 0; y < n; y++)
+	{
+		for (x = 0; x < n; x++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
 }
